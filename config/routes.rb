36f393120adbd6e07080574441df2fd899c820ba
Rick Smoke <rsmoke@umich.edu>
2018-12-18 17:38:38 -0500

    ADD: device and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  get 'static_pages/index'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/privacy'
+  get 'static_pages/terms_of_use'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
