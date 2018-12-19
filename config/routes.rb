bad6fe61425c02eb0fb55e8f97bcfe46969df792
Rick Smoke <rsmoke@umich.edu>
2018-12-19 17:17:04 -0500

    UPDATE: static pages and layout helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  get 'static_pages/index'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/privacy'
-  get 'static_pages/terms_of_use'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/privacy', to: 'static_pages#privacy'
+  get '/terms_of_service', to: 'static_pages#terms_of_use'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
