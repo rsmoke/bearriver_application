6895c6110cdc12ced41cd46e8418a6d93453032a
Rick Smoke <rsmoke@umich.edu>
2019-01-07 15:53:46 -0500

    update puma profile bits

--- config/puma_prod.rb
+++ config/puma_prod.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env puma
 
-directory '/home/deployer/apps/nelp_application/current'
-rackup "/home/deployer/apps/nelp_application/current/config.ru"
+directory '/home/deployer/apps/bearriver_application/current'
+rackup "/home/deployer/apps/bearriver_application/current/config.ru"
 environment 'production'
 
-pidfile "/home/deployer/apps/nelp_application/shared/tmp/pids/puma.pid"
-state_path "/home/deployer/apps/nelp_application/shared/tmp/pids/puma.state"
-stdout_redirect '/home/deployer/apps/nelp_application/current/log/puma.error.log', '/home/deployer/apps/nelp_application/current/log/puma.access.log', true
+pidfile "/home/deployer/apps/bearriver_application/shared/tmp/pids/puma.pid"
+state_path "/home/deployer/apps/bearriver_application/shared/tmp/pids/puma.state"
+stdout_redirect '/home/deployer/apps/bearriver_application/current/log/puma.error.log', '/home/deployer/apps/bearriver_application/current/log/puma.access.log', true
 
 threads 4,16
 
-bind 'unix:///home/deployer/apps/nelp_application/shared/tmp/sockets/nelp_application-puma.sock'
+bind 'unix:///home/deployer/apps/bearriver_application/shared/tmp/sockets/bearriver_application-puma.sock'
 
 workers 0
 
 preload_app!
 on_restart do
   puts 'Refreshing Gemfile'
-  ENV["BUNDLE_GEMFILE"] = "/home/deployer/apps/nelp_application/current/Gemfile"
+  ENV["BUNDLE_GEMFILE"] = "/home/deployer/apps/bearriver_application/current/Gemfile"
 end
 
 before_fork do
