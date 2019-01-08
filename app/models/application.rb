a85b52491cef2d9a35482e3486c1efe41bde8025
Rick Smoke <rsmoke@umich.edu>
2019-01-08 16:35:45 -0500

    ADD: validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,21 @@
 class Application < ApplicationRecord
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :gender, presence: true
+  validates :birth_year, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :country, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
+  validates :workshop_selection1, presence: true
+  validates :workshop_selection2, presence: true
+  validates :workshop_selection3, presence: true
+  validates :lodging_selection, presence: true
+  validates :partner_registration_selection, presence: true
+  
   belongs_to :user
 end
