e5cb3e14fee41e4a4a4451b86b717e93dbcd7a8f
Rick Smoke <rsmoke@umich.edu>
2019-01-10 17:33:00 -0500

    ADD: partner_registration flow

---
+++ app/models/partner_registration.rb
@@ -0,0 +1,2 @@
+class PartnerRegistration < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_210314) do
+ActiveRecord::Schema.define(version: 2019_01_10_185924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,13 @@ ActiveRecord::Schema.define(version: 2019_01_09_210314) do
     t.datetime "updated_at", null: false
     t.bigint "reservation_id"
     t.index ["reservation_id"], name: "index_lodgings_on_reservation_id"
+  end
+
+  create_table "partner_registrations", force: :cascade do |t|
+    t.string "description"
+    t.decimal "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "payments", force: :cascade do |t|
