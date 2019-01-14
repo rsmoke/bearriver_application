0d97297b69f7af557883ef44b9bf486a6264b80a
Rick Smoke <rsmoke@umich.edu>
2019-01-13 20:58:43 -0500

    ADD: admin to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
+private
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
 
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
+
+  def contest_is_open?
+    if ApplicationSetting.current_app_settings.opendate > Time.now
+      redirect_to conference_closed_url
+    end
+  end
+
+  def contest_is_full?
+    if ApplicationSetting.current_app_settings.application_buffer < Payment.where(transactionStatus: "1").count
+      redirect_to conference_full_url
+    end
+  end
 end
