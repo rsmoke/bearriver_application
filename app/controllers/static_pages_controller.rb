ba370f6112a71b9dcc2aa6f2934289c80ebe624e
Rick Smoke <rsmoke@umich.edu>
2019-01-13 21:00:22 -0500

    ADD: before_actions to route per application settings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+  before_action :set_application_settings
+  before_action :contest_is_open?, only: [:index]
+  before_action :contest_is_full?, only: [:index]
+
   def index
   end
 
@@ -12,5 +16,20 @@ class StaticPagesController < ApplicationController
   end
 
   def terms_of_use
+  end
+
+  def conference_closed
+    @current_year = @app_settings.contest_year
+    @opens_at = @app_settings.opendate
+  end
+
+  def conference_full
+  end
+
+
+private
+
+  def set_application_settings
+    @app_settings = ApplicationSetting.current_app_settings
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,15 @@ Rails.application.routes.draw do
     resources :lodgings
     resources :workshops
   end
-
+  resources :application_settings
   devise_for :users
 
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/privacy', to: 'static_pages#privacy'
   get '/terms_of_service', to: 'static_pages#terms_of_use'
-
+  get '/conference_closed', to: 'static_pages#conference_closed'
+  get '/conference_full', to: 'static_pages#conference_full'
 
   get 'payment_receipt', to: 'payments#payment_receipt'
   get 'make_payment', to: 'payments#make_payment'
