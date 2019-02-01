1a9c7a32b5162a487133724b5ce0a3116d573f7b
Rick Smoke <rsmoke@umich.edu>
2019-02-01 14:57:22 -0500

    UPDATE: who gets sent to dev nelenet

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -51,7 +51,7 @@
         user_account = current_user.email.partition('@').first + '-' + current_user.id.to_s
         redirect_url = 'https://lsa-english-bearriver.miserver.it.umich.edu/payment_receipt'
         amount_to_be_payed = amount.to_i
-        if Rails.env.development? || current_user.id == 1
+        if Rails.env.development? || current_user.id <= 3
            key_to_use = 'test_key'
            url_to_use = 'test_URL'
          else
