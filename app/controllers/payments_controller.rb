526cc534755635fd6bdecf205d662c2a0f490b52
Rick Smoke <rsmoke@umich.edu>
2019-02-02 15:16:25 -0500

    UPDATE: show payments methid logic

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -38,6 +38,7 @@
     end
 
     def payment_show
+      redirect_to root_url unless user_has_payments?(current_user)
       @users_current_payments = Payment.where(user_id: current_user )
       @ttl_paid = Payment.where(user_id: current_user, transaction_status: '1').pluck(:total_amount).map(&:to_f).sum / 100
       cost_lodging = Lodging.find(current_user.application.lodging_selection).cost.to_f
