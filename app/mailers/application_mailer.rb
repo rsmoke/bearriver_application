13080f81355bc1b695e9b95dc4b6fde93a0df974
Rick Smoke <rsmoke@umich.edu>
2020-01-23 19:16:59 -0500

    Add mailers for lottery

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'bearriver-questions@umich.edu'
   layout 'mailer'
 end
---
+++ app/mailers/lottery_mailer.rb
@@ -0,0 +1,16 @@
+class LotteryMailer < ApplicationMailer
+
+  def won_lottery_email
+    @application = params[:application]
+    @current_application_settings = ApplicationSetting.get_current_app_settings
+    @url = 'https://lsa-english-bearriver.miserver.it.umich.edu/payment_show'
+    mail(to: @application.email, subject: "You have been selected by lottery for a place in the 
+      #{@current_application_settings.contest_year} Bear River Writers’ Conference.")
+  end
+
+  def lost_lottery_email  
+    @application = params[:application]
+    @current_application_settings = ApplicationSetting.get_current_app_settings
+    mail(to: @application.email, subject: "#{@current_application_settings.contest_year} Bear River Writers’ Conference lottery result")
+  end
+end
