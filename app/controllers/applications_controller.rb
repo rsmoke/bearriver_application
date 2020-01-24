5643aa0254e470cde73002a935e37e006bf2d537
Rick Smoke <rsmoke@umich.edu>
2020-01-23 19:23:04 -0500

    Fix authorization check

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -78,7 +78,7 @@ class ApplicationsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_application
       @application = Application.find(params[:id])
-      redirect_to root_url, alert: 'Not Authorized!' unless @application.user === current_user || current_user_admin?
+      redirect_to root_url, alert: 'Not Authorized!' unless @application.user === current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
