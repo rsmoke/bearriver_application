7761714d1033d3d9dc943f18489a320a22cedee3
Rick Smoke <rsmoke@umich.edu>
2020-01-30 17:04:38 -0500

    ADD dependent destroy for appliocations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :validatable
 
   has_many :payments, dependent: :destroy
-  has_one :application #, dependent: :destroy
+  has_one :application, dependent: :destroy
 
   def total_paid
     payments.pluck(:total_amount).map{ |v| v.to_f }.sum / 100
