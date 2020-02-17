13d55ee0aa74c469dd41cf46bcc12ad4f04aa17d
Rick Smoke <rsmoke@umich.edu>
2020-02-17 10:11:41 -0500

    rearrange recent X columns

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -24,19 +24,9 @@ ActiveAdmin.register_page "Dashboard" do
 
       column do
         panel "Waiting for responses from these applicants" do
-          table_for Application.application_offered.order("id desc").limit(10) do
+          table_for Application.application_offered.order("id desc").limit(50) do
             column("User") { |u| link_to(u.user.email, admin_application_path(u.id)) }
             column("Offer Date") { |od| od.offer_status_date }
-          end
-        end
-      end
-
-      column do
-        panel "Recent Payments" do
-          table_for Payment.order("id desc").limit(10) do
-            column("User") { |u| link_to(u.user.email, admin_user_path(u.user)) }
-            # column("Customer") { |order| link_to(order.user.email, admin_user_path(order.user)) }
-            column("Amount") { |order| number_to_currency order.total_amount.to_f / 100 }
           end
         end
       end
@@ -55,6 +45,16 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Recent Applications" do
           table_for Application.order("id desc").limit(10).each do |app|
             column(:id) { |app| link_to(app.name, admin_application_path(app)) }
+          end
+        end
+      end
+
+      column do
+        panel "Recent Payments" do
+          table_for Payment.order("id desc").limit(10) do
+            column("User") { |u| link_to(u.user.email, admin_user_path(u.user)) }
+            # column("Customer") { |order| link_to(order.user.email, admin_user_path(order.user)) }
+            column("Amount") { |order| number_to_currency order.total_amount.to_f / 100 }
           end
         end
       end
