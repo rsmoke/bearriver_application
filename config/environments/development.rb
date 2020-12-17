a99396f2fb5a9994962dc6e398a42dd68d642b40
Rick Smoke <rsmoke@umich.edu>
2020-12-17 16:11:57 -0500

    change to letter_opener_web and configure

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   #letter_opener settings
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,8 @@ Rails.application.routes.draw do
 
   post '/send_offer/:id', to: 'application_settings#send_offer', as: 'send_offer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
