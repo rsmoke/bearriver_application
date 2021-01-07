b17f20916c10b451215f2982c57e30cdf65675a0
Rick Smoke <rsmoke@umich.edu>
2021-01-07 15:55:59 -0500

    add lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-# lock "~> 3.14.0"
+lock "~> 3.14.1"
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.7.1'
 
