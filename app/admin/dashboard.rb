6777ca357f345a762d8b3713bc989a15c0f42548
Rick Smoke <rsmoke@umich.edu>
2021-01-12 15:42:13 -0500

    cleanup dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -14,7 +14,35 @@ ActiveAdmin.register_page "Dashboard" do
 
     columns do
       column do
-        panel "Applicants who accepted their registration offer" do
+        panel "Recent #{ApplicationSetting.get_current_app_year} Applications" do
+          table_for Application.active_conference_applications.order("id desc").limit(10).each do |app|
+            column(:id) { |app| link_to(app.name, admin_application_path(app)) }
+          end
+        end
+      end
+
+      column do
+        panel "Recent Payments" do
+          table_for Payment.current_conference_payments.limit(10) do
+            column("User") { |u| link_to(u.user.email, admin_user_path(u.user)) }
+            column("Amount") { |order| number_to_currency order.total_amount.to_f / 100 }
+            
+          end
+        end
+      end
+
+      column do
+        panel "Recent Users" do
+          table_for User.order("id desc").limit(10).each do |user|
+            column(:email) { |user| link_to(user.email, admin_user_path(user)) }
+          end
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "#{ApplicationSetting.get_current_app_year} Applicants who accepted their registration offer" do
           table_for Application.application_accepted.order("id desc").limit(110) do
             column("User") { |u| link_to(u.user.email, admin_application_path(u.id)) }
             column("Offer Date") { |od| od.offer_status_date }
@@ -23,7 +51,7 @@ ActiveAdmin.register_page "Dashboard" do
       end
 
       column do
-        panel "Waiting for responses from these applicants" do
+        panel "Waiting for responses from these #{ApplicationSetting.get_current_app_year} applicants" do
           table_for Application.application_offered.order("id desc").limit(50) do
             column("User") { |u| link_to(u.user.email, admin_application_path(u.id)) }
             column("Offer Date") { |od| od.offer_status_date }
@@ -31,33 +59,5 @@ ActiveAdmin.register_page "Dashboard" do
         end
       end
     end # columns
-
-    columns do
-      column do
-        panel "Recent Users" do
-          table_for User.order("id desc").limit(10).each do |user|
-            column(:email) { |user| link_to(user.email, admin_user_path(user)) }
-          end
-        end
-      end
-
-      column do
-        panel "Recent Applications" do
-          table_for Application.order("id desc").limit(10).each do |app|
-            column(:id) { |app| link_to(app.name, admin_application_path(app)) }
-          end
-        end
-      end
-
-      column do
-        panel "Recent Payments" do
-          table_for Payment.order("id desc").limit(10) do
-            column("User") { |u| link_to(u.user.email, admin_user_path(u.user)) }
-            # column("Customer") { |order| link_to(order.user.email, admin_user_path(order.user)) }
-            column("Amount") { |order| number_to_currency order.total_amount.to_f / 100 }
-          end
-        end
-      end
-    end
   end # content
 end
