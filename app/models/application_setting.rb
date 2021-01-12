00142fc84d6d61b91c8bc82d754f9da2dcbe1ad1
Rick Smoke <rsmoke@umich.edu>
2021-01-12 15:08:22 -0500

    added current app scope

--- app/models/application_setting.rb
+++ app/models/application_setting.rb
@@ -33,10 +33,8 @@ class ApplicationSetting < ApplicationRecord
   validates :registration_fee, presence: true, numericality: true
   validates :lottery_buffer, presence: true, numericality: true
   validates :application_open_period, presence: true, numericality: { only_integer: true }
-  # validates :active_application, presence: true
-  # has_rich_text :application_open_directions
-  # has_rich_text :application_closed_directions
-  # scope :current_app_settings, -> { find_by(contest_year: Date.today.cwyear) }
+
   scope :get_current_app_settings, -> { where("active_application = ?", true).max }
+  scope :get_current_app_year, -> { get_current_app_settings.contest_year }
 
 end
